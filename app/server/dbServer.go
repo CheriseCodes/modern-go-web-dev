a616d434cc5c24f609d7bed0c1fd1ac6fa9f3673
CheriseCodes <cherise.bry@gmail.com>
2023-12-05 10:50:47 -0500

    add dbserver

---
+++ app/server/dbServer.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"database/sql"
+	"log"
+)
+
+func InitDatabase(config *viper.Viper) *sql.DB {
+	connectionString := config.GetString("database.connection_string")
+	maxIdleConnections := config.GetInt("database.max_idle_connections")
+	maxOpenConnections := config.GetInt("database.max_open_connections")
+	connectionMaxLifetime := config.GetDiration("database.connection_max_lifetime")
+	driverName := config.GetString("database.driver_name")
+	if connectionString == "" {
+		log.Fatalf("Database connection string missing")
+	}
+	dbHandler, err := sql.Open(driverName, connectionString)
+	if err != nil {
+		log.Fatalf("Error while initializing database: %v", err)
+	}
+	dbHandler.SetMaxIdleConns(maxIdleConnections)
+	dbHandler.SetMaxOpenConns(maxOpenConnections)
+	dbHandler.SetConnMaxLifetime(connectionMaxLifetime)
+	err = dbHandler.Ping()
+	if err != nil {
+		dbHandler.Close()
+		log.Fatalf("Error while validating database: %v", err)
+	}
+	return dbHandler
+}
