2d1c8d6bef262879476d4b60caef1d8510322938
CheriseCodes <cherise.bry@gmail.com>
2023-12-17 12:59:11 -0500

    correct tests

--- app/controllers/runnersController_test.go
+++ app/controllers/runnersController_test.go
@@ -30,13 +30,13 @@ func TestGetRunnersResponse(t *testing.T) {
 	dbHandler, mock, _ := sqlmock.New()
 	defer dbHandler.Close()
 	columnsUsers := []string{"user_role"}
-	mock.ExpectQuery("SELECT user_role").WillReturnRows(sqlmock.NewRows(columnsUsers).AddRow("runner"))
+	mock.ExpectQuery("SELECT user_role FROM users WHERE access_token = ?").WithArgs("token").WillReturnRows(sqlmock.NewRows(columnsUsers).AddRow("runner"))
 	columns := []string{"id", "first_name", "last_name", "age", "is_active", "country", "personal_best", "season_best"}
 	mock.ExpectQuery("SELECT *").WillReturnRows(sqlmock.NewRows(columns).AddRow("1", "John", "Smith", 30, true, "United States", "02:00:41", "02:13:13").AddRow("2", "Marijana", "Komatinovic", 30, true, "Serbia", "01:18:28", "01:18:28"))
 	router := initTestRouter(dbHandler)
 	request, _ := http.NewRequest("GET", "/runner", nil)
 	recorder := httptest.NewRecorder()
-	request.Header.Set("token", "token")
+	request.Header.Set("Token", "token")
 	router.ServeHTTP(recorder, request)
 	assert.Equal(t, http.StatusOK, recorder.Result().StatusCode)
 	var runners []*models.Runner
