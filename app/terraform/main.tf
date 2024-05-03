1c3fe97ac6de9572693a8749e0337e07c69bcfd1
CheriseCodes <cherise.bry@gmail.com>
2024-05-02 22:21:47 -0400

    gcp config in terraform

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -1,7 +1,50 @@
-provider "google" {
-  project = "tutorials-421820"
-  region  = "northamerica-northeast2"
-  zone    = "northamerica-northeast2-c"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.27.0"
+    }
+  }
 }
 
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
 
+resource "google_container_cluster" "primary" {
+  name                = "runners-cluster"
+  location            = var.zone
+  initial_node_count  = 3
+  deletion_protection = false
+  node_config {
+    disk_size_gb = 10
+    machine_type = "e2-micro"
+  }
+}
+
+resource "google_sql_database" "database" {
+  name     = "runners_db"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_database_instance" "instance" {
+  name                = "database-instance"
+  root_password       = var.root_password
+  deletion_protection = false
+  database_version    = "POSTGRES_15"
+
+  settings {
+    tier              = "db-f1-micro"
+    disk_type         = "PD_HDD"
+    availability_type = "ZONAL"
+    edition           = "ENTERPRISE"
+    backup_configuration {
+      enabled = false
+    }
+    data_cache_config {
+      data_cache_enabled = false
+    }
+  }
+}
